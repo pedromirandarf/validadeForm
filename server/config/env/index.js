50bb0f28a56c8864f526974a9a8e5f903e469d77
pedromirandarf <pedro.celsomrf@gmail.com>
2020-06-15 09:07:13 -0300

    Alteração para API

---
+++ server/config/env/index.js
@@ -0,0 +1,17 @@
+module.exports = {
+    env: 'development',
+    nomeApi: 'RicardoC',
+    urlApi: 'http://localhost',
+    portApi: 3303,
+    dataConfig: {
+      Postgres: {
+        database: 'yyakiuxn',
+        host: 'localhost',
+        port: '5432',
+        user: 'yyakiuxn',
+        password: 'GmU0aIe5Q3debKl6rygQvWakzOBAZ4b1',
+        url: "postgres://yyakiuxn:GmU0aIe5Q3debKl6rygQvWakzOBAZ4b1@motty.db.elephantsql.com:5432/yyakiuxn"
+      }
+    }
+  };
+  
